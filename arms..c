123ebf140a8df8d0a7d59bd2e5086f9c7831f125
selvarajrojaajith <36071813+selvarajrojaajith@users.noreply.github.com>
2018-02-21 15:56:41 +0530

    arms.c
    good

---
+++ arms..c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int number,remainder;
+printf("\n Enter the two digits number");
+scanf("%d",&numbeer);
+original number=number;
+{
+remainder=original number;
+if(result==number)
+printf("\n Enter the armstrong number");
+else
+printf("\n Enter the not armstrong number");
+return 0
+}
